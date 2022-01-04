dae57e9de293f3c3ccbddd352351d0900d1e602d
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-05 00:12:42 +0200

    Update install.sh

--- software/install.sh
+++ software/install.sh
@@ -6,9 +6,11 @@ wget $URL
 
 mkdir /opt/deepcool_rgb
 
-mv deepcool_rgb /opt/deepcool_rgb/deepcool_daemon
+mv linux_rgb_daemon /opt/deepcool_rgb/deepcool_daemon
 chmod 755 /opt/deepcool_rgb/deepcool_daemon
 
 echo /opt/deepcool_rgb/deepcool_daemon $PORT > /usr/bin/deepcool_rgb 
 chmod 755 /usr/bin/deepcool_rgb
 echo add to startup from gui 
+rm -rf linux_rgb_daemon
+rm -rf install.sh
